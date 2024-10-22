9dd88d9c0b70b3c17e31857af88e84a983056870
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-10-22 16:36:52 +0300

    8. List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,10 +2,10 @@
 
 """Module defines the `list_all` method"""
 
+
 from typing import List
-from pymongo.collection import Collection
 
 
-def list_all(mongo_collection: Collection) -> List:
+def list_all(mongo_collection) -> List:
     """Returns list of documents in collection"""
     return list(mongo_collection.find())
