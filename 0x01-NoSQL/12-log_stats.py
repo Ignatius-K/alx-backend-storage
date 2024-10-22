094e89e2e090c587824d7081e4a12514a6f4dcc0
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-10-22 19:28:46 +0300

    12. Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""Script provides stats of Nginx logs in MongoDB"""
+
+
+from typing import Union
+from pymongo import MongoClient
+
+
+def create_connection(host: str = "127.0.0.1", port: Union[int, str] = 27017):
+    """Returns connection to nginx database"""
+    db = MongoClient(host=host, port=port)
+    return db.logs
+
+
+def print_log_stats(session, collection_name: str = "nginx") -> None:
+    """Print the logs stats
+
+    Args:
+        session: The database session
+        collection_name: The name of the collection
+    """
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    logs = session[collection_name]
+
+    print(f"{logs.count_documents({})} logs\nMethods:")
+    for method in methods:
+        print(f"\tMethod {method}: " +
+              f"{logs.count_documents({'method': method})}"
+              )
+    print(f"{logs.count_documents({'method': 'GET', 'path': '/status'})} " +
+          f"status check"
+          )
+
+
+if __name__ == '__main__':
+    session = create_connection()
+    if not session:
+        exit(code=1)
+    print_log_stats(session)
