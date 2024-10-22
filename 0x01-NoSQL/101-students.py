d8884bbaef67958547ef00e4cda557b4af010018
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-10-22 23:57:33 +0300

    14. Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Module defines the `top_students` method"""
+
+
+from typing import Sequence
+from pymongo.collection import Collection # type: ignore
+
+
+def top_students(mongo_collection: Collection) -> Sequence:
+    """Gets top students by average score
+
+    Args:
+        mongo_collection: The MongoDB collection
+
+    Returns:
+        A list of top students
+    """
+    add_average_score_field_stage = {
+        '$addFields': {
+            'averageScore': {'$avg': '$topics.score'}
+        }
+    }
+    sort_stage = {
+        'averageScore': -1
+    }
+    aggregator_pipeline = [
+        add_average_score_field_stage,
+        sort_stage
+    ]
+    return list(mongo_collection.aggregate(aggregator_pipeline))
