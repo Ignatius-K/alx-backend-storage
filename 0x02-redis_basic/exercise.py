714ce81a0354debd6ca354926b24c0a433f68362
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-10-26 11:24:41 +0300

    1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,11 +3,22 @@
 """Module define the Cache service"""
 
 import redis
-from typing import Union
+from typing import Callable, Optional, TypeVar, Union, cast
 import uuid
 
 
+T = TypeVar('T')
 CacheDataType = Union[str, bytes, int, float]
+
+
+def get_str(data: bytes) -> str:
+    """Gets str from bytes"""
+    return data.decode(encoding="utf-8")
+
+
+def get_int(data: bytes) -> int:
+    """Gets int from bytes"""
+    return int(get_str(data))
 
 
 class Cache(object):
@@ -41,13 +52,33 @@ class Cache(object):
         Return:
             (UUID) The key to the data in the store
         """
-        key = self.get_key()
+        key = self.generate_data_key()
         with self._redis.pipeline() as pipe:
             pipe.set(key, data)
             pipe.execute()
         return key
 
-    def get_key(self) -> str:
+    def get(
+        self, key: str, fn: Optional[Callable[[bytes], T]] = None
+    ) -> Union[T, bytes]:
+        """Gets data from the Cache
+
+        Args:
+            key: The data key
+            fn: The function to format the data
+        """
+        byte_data = cast(bytes, self._redis.get(key))
+        return fn(byte_data) if fn is not None else byte_data
+
+    def get_str(self, key: str) -> str:
+        """Gets string value from cache"""
+        return cast(str, self.get(key, lambda x: x.decode(encoding="utf-8")))
+
+    def get_int(self, key: str) -> int:
+        """Gets int data from cache"""
+        return int(self.get_str(key))
+
+    def generate_data_key(self) -> str:
         """Gets a new key"""
         while True:
             key = str(uuid.uuid4())
